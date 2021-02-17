4073c4631c639108b5abc35005138a0edfcd88e5
Blixten37 <60793957+BlixtDev@users.noreply.github.com>
2021-02-17 19:40:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from twill.commands import go
-
 print("--~~ [ Discord Spam Bot Script ] ~~--")
 print(""), print("This bot is under the MIT License, read more at: https://en.wikipedia.org/wiki/MIT_License")
 print(""), print("DiscordSpamBotScript (C) 2021 Blixten37"), print("")
 
-import os
-print("[DSBS] | INFO: Succesfully imported os")
-import sys
-print("[DSBS] | INFO: Succesfully imported sys")
 import random
 print("[DSBS] | INFO: Succesfully imported random")
-import json
-print("[DSBS] | INFO: Succesfully imported json")
-import requests
-print("[DSBS] | INFO: Succesfully imported requests")
 import pyautogui
 print("[DSBS] | INFO: Succesfully imported pyautogui")
 import time
-print("[DSBS] | INFO: Succesfully imported time"), print("")
+print("[DSBS] | INFO: Succesfully imported time")
+import tkinter
+print("[DSBS] | INFO: Succesfully imported tkinter"), print("")
 
 
 time.sleep(1), print("1")
@@ -39,16 +31,14 @@ loop = "true"
 for word in f:
     while loop == "true":
         time.sleep(1)
-        pyautogui.typewrite(random.choice(open("spam").read().split(),))
-        pyautogui.typewrite(" ")
+        pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
 
-        pyautogui.typewrite(random.choice(open("spam").read().split(),))
-        pyautogui.typewrite(" ")
+        pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
 
-        pyautogui.typewrite(random.choice(open("spam").read().split(),))
-        pyautogui.typewrite(" ")
+        pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
 
-        pyautogui.typewrite(random.choice(open("spam").read().split(),))
-        pyautogui.typewrite(" ")
+        pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
 
-        pyautogui.press("enter"), print(f"sent some spam!")
+        pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
+
+        pyautogui.press("enter")
