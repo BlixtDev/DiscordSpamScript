a5b20e672a93e6e5e6508616d9c508bb15482bef
Blixten37 <60793957+BlixtDev@users.noreply.github.com>
2021-02-18 16:43:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ print(""), print("This bot is under the MIT License, read more at: https://en.wi
 print(""), print("DiscordSpamBotScript (C) 2021 Blixten37"), print("")
 
 import random
-print("[TSB] | INFO: Succesfully imported random")
+
+print("[TSB] | INFO: Successfully imported random")
 import pyautogui
-print("[TSB] | INFO: Succesfully imported pyautogui")
+
+print("[TSB] | INFO: Successfully imported pyautogui")
 import time
-print("[TSB] | INFO: Succesfully imported time")
+
+print("[TSB] | INFO: Successfully imported time")
 import tkinter as tk
-print("[TSB] | INFO: Succesfully imported tkinter"), print("")
+
+print("[TSB] | INFO: Successfully imported tkinter"), print("")
 
 root = tk.Tk()
 root.title("Text Spam Bot")
 
 loop = "false"
+
 
 def onEnable():
     loop = "true"
@@ -24,15 +29,15 @@ def onEnable():
     for word in f:
         while loop == "true":
             time.sleep(1)
-            pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
+            pyautogui.typewrite(random.choice(open("spam").read().split(), )), pyautogui.typewrite(" ")
 
-            pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
+            pyautogui.typewrite(random.choice(open("spam").read().split(), )), pyautogui.typewrite(" ")
 
-            pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
+            pyautogui.typewrite(random.choice(open("spam").read().split(), )), pyautogui.typewrite(" ")
 
-            pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
+            pyautogui.typewrite(random.choice(open("spam").read().split(), )), pyautogui.typewrite(" ")
 
-            pyautogui.typewrite(random.choice(open("spam").read().split(),)), pyautogui.typewrite(" ")
+            pyautogui.typewrite(random.choice(open("spam").read().split(), )), pyautogui.typewrite(" ")
 
             pyautogui.press("enter")
 
@@ -41,9 +46,9 @@ def onDisable():
     loop = "false"
     print("Disabled spam")
 
-Btn1 = tk.Button(root, text="Start", command=onEnable)
-Btn2 = tk.Button(root, text="Stop", command=onDisable)
 
-Btn1.pack()
-Btn2.pack()
+Btn1 = tk.Button(root, text="Start", command=onEnable).pack()
+Btn2 = tk.Button(root, text="Stop", command=onDisable).pack()
+
+
 root.mainloop()
